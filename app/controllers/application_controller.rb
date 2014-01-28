6b7c478bc340d8ed72e4b51c6951e8e0f7cad0ba
wujysh <wujysh@gmail.com>
2014-01-29 00:01:14 +0800

    Fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :null_session
   #skip_before_filter :verify_authenticity_token
+  protect_from_forgery :except => :create 
 end
